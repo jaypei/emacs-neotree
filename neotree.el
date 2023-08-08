53c0e55034644236ee3761037e0dcfcd1c881c45
Abdisalan Mohamud <abdi.mohamud94@gmail.com>
2023-08-07 20:37:30 -0600

    Bump version to 0.6.0

--- neotree.el
+++ neotree.el
@@ -4,7 +4,7 @@
 
 ;; Author: jaypei <jaypei97159@gmail.com>
 ;; URL: https://github.com/jaypei/emacs-neotree
-;; Version: 0.5.1
+;; Version: 0.6.0
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
